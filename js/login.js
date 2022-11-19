5e91df7ae32959055fff2095f11e6234b651b312
rvmatias <ruizmatias.99@gmail.com>
2022-11-19 00:15:39 -0300

    se modificio el login

--- js/login.js
+++ js/login.js
@@ -1,26 +1,65 @@
+document.getElementById("btn__registrarse").addEventListener("click",register);
+document.getElementById("btn__iniciar-sesion").addEventListener("click",iniciarSesion);
 
-function obtenerListaUsuarios() {
-    var listaUsuarios = JSON.parse(localStorage.getItem(`ListaUsuariosLs`));
-    if(listaUsuarios  == null) {
-        listaUsuarios =
-        [
-           ['1','matias', 'Ruiz', 'ruizmatias.99@gmail.com', '123456','1'],
-           ['2', 'agustin', 'zelaya', 'agustinzelayacossio@gmail.com', '654321','2'], 
-        ]
+window.addEventListener("resize", anchoPagina);
+
+// Declarando variables
+var contenedor_login_register=document.querySelector(".contenedor__login-register");
+var formulario_login=document.querySelector(".formulario__login");
+var formulario_register=document.querySelector(".formulario__register");
+var caja_trasera_login=document.querySelector(".caja__trasera-login");
+var caja_trasera_register=document.querySelector(".caja__trasera-register");
+
+function anchoPagina(){
+    if(window.innerWidth > 850){
+        caja_trasera_login.style.display = "block";
+        caja_trasera_register.style.display= "block";
+    }else{
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+    }
+
+}
+anchoPagina();
+
+function iniciarSesion(){
+    if(window.innerWidth > 850){
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "10px";
+        formulario_login.style.display="block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity="0";
+
+    }else{
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display="block";
+        caja_trasera_register.style.display ="block";
+        caja_trasera_login.style.display="none";
 
     }
-    return listaUsuarios;
-}
-
-function validarCredenciales(pCorreo , pContraseña) {
-    var listaUsuarios = obtenerListaUsuarios();
-    var bAcceso = false;
-
-    for(var i = 0; i < listaUsuarios.length; i++ ){
-        if(pCorreo == listaUsuarios[i][3] && pContraseña == listaUsuarios [i][4]){
-            bAcceso = true;
-            sessionStorage.setItem('usuarios', listaUsuarios[i][1]+""+listaUsuarios[i][2]);
-        }
     }
-    return bAcceso;
-}
+
+function register(){
+    if(window.innerWidth > 850){
+        formulario_register.style.display = "block";
+    contenedor_login_register.style.left = "410px";
+    formulario_login.style.display="none";
+    caja_trasera_register.style.opacity = "0";
+    caja_trasera_login.style.opacity="1";
+
+    } else{
+    formulario_register.style.display = "block";
+    contenedor_login_register.style.left = "0px";
+    formulario_login.style.display="none";
+    caja_trasera_register.style.display = "none";
+    caja_trasera_login.style.display="block";
+    caja_trasera_login.style.opacity="1";
+
+    }
+    
+    }
